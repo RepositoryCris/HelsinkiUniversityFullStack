e9c7bdfeac55aa9b00225618cf410846ce2ca771
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-03-05 21:42:09 -0400

    E1.2CourseInformation Step 2 Content Refactor

--- part1/E1.1CourseInformation/src/App.jsx
+++ part1/E1.1CourseInformation/src/App.jsx
@@ -4,6 +4,16 @@ const Header = (props)=>{
   )
 }
 
+const Content = (props)=>{
+  return (
+    <div>
+      <p> {props.part1} {props.exercises1} </p>
+      <p> {props.part2} {props.exercises2} </p>
+      <p> {props.part3} {props.exercises3} </p>
+    </div>
+  )
+}
+
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -17,6 +27,13 @@ const App = () => {
   return (
     <div>
       <Header name = { course }/>
+      <Content
+        part1 = { part1 }
+        part2 = { part2 }
+        part3 = { part3 }
+        exercises1 = { exercises1 }
+        exercises2 = { exercises2 }
+        exercises3 = { exercises3 }/>
     </div>
   )
 }
