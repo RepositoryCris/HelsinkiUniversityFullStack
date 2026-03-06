5d1b9859d4f9a55e8f114b30441403eb539ea117
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-03-06 17:59:01 -0400

    E1.6Unicafe Step 1 Complete UI with increment buttons

--- part1/E1.6Unicafe/src/App.jsx
+++ part1/E1.6Unicafe/src/App.jsx
@@ -1,5 +1,28 @@
 import { useState } from 'react'
 
+const Header = ()=>{
+  return (<h1>Give feedback</h1>)
+}
+
+const Button = ({ name , onClick})=>{
+  return (
+  <>
+    <button onClick={onClick}>{ name }</button>
+  </>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +31,13 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header />
+      <Button name = "good" onClick = { () => setGood(good+1) }/>
+      <Button name = "neutral" onClick = { () => setNeutral(neutral+1) }/>
+      <Button name = "bad" onClick = { () => setBad(bad+1) }/>
+      <Statistics good = { good }
+                  neutral ={ neutral }
+                  bad = { bad } />
     </div>
   )
 }
