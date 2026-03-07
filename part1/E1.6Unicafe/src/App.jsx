1e1058cda852d4825848be6f1b90a649e4f54d36
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-03-06 21:09:43 -0400

    E1.11Unicafe Step 6 Display the statistics in an HTML table

--- part1/E1.6Unicafe/src/App.jsx
+++ part1/E1.6Unicafe/src/App.jsx
@@ -12,16 +12,17 @@ const Button = ({ name , onClick})=>{
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      { text } { value}
-    </p>
+        <tr>
+          <td>{ text }</td>
+          <td>{ value }</td>
+        </tr>
   )
 }
 
 const Statistics = ({ good, neutral, bad }) => {
-  const total = parseInt(good + neutral + bad)
-  const average = total === 0 ? 0 : parseFloat((good*(1) + neutral*(0) + bad*(-1))/total)
-  const positive = total === 0 ? 0 : parseFloat((good/total)*100)
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : ((good - bad)/total)
+  const positive = total === 0 ? 0 : (good/total)*100
 
   // If no feedback, show message
   if (total === 0) {
@@ -36,13 +37,17 @@ const Statistics = ({ good, neutral, bad }) => {
   // Otherwise show statistics
   return (
     <>
-    <h2>Statistics</h2>
-    <StatisticLine text="good" value={good} />
-    <StatisticLine text="neutral" value={neutral} />
-    <StatisticLine text="bad" value={bad} />
-    <StatisticLine text="all" value={total} />
-    <StatisticLine text="average" value={average.toFixed(2)} />
-    <StatisticLine text="positive" value={positive.toFixed(2) + "%"} />
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average.toFixed(2)} />
+        <StatisticLine text="positive" value={positive.toFixed(2) + "%"} />
+        </tbody>
+      </table>
     </>
   )
 }
