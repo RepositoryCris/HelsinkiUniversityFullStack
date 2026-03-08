744d418a1c6a61399b305e07439465e3f31d2989
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-03-08 17:10:18 -0400

    E2.9 The Phonebook Step 4: Add search field to filter the list of people by name

--- part2/E2.6ThePhonebook/src/App.jsx
+++ part2/E2.6ThePhonebook/src/App.jsx
@@ -5,27 +5,41 @@ const ShowPerson = ({ name, number }) => <p>{ name } { number }</p>
 const Content = ({ persons }) => (
   <>
     {persons.map(person => (
-      <ShowPerson key={person.name} name={person.name} number = {person.number} />
+      <ShowPerson key = {person.id}
+                  name = {person.name}
+                  number = {person.number} />
     ))}
   </>
 )
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-12344567" }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
   
   const handleNameInput  = ({ target }) => setNewName(target.value)
   const handleNumberInput  = ({ target }) => setNewNumber(target.value)
+  const handleFilterInput = ({ target }) => setFilter(target.value)
+
+  const personsToShow = filter === ''
+    ? persons
+    : persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
 
   const addPerson = ( event ) => {
     event.preventDefault()
 
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     
     const nameExists = persons.some((person) => person.name.toLowerCase() === newName.toLowerCase())
@@ -47,7 +61,13 @@ const App = () => {
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Filter shown with:
+        <input onChange = { handleFilterInput } value = { filter }/>
+      </div>
+
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange = { handleNameInput } value = { newName }/>
@@ -60,7 +80,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Content persons = { persons }/>
+      <Content persons = { personsToShow }/>
     </div>
   )
 }
