44ea97cf64f660fc4392eeea18f91b9350746f6e
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-03-07 21:12:50 -0400

    E2.5 Course Information Step 10: Separate module (Course component)

--- part2/E2.1CourseInformation/src/App.jsx
+++ part2/E2.1CourseInformation/src/App.jsx
@@ -1,31 +1,4 @@
-const Header = ({ name }) => <h2>{ name }</h2>
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map(part => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-)
-
-const Part = ({ part }) => (
-      <p>{part.name} {part.exercises}</p>
-  )
-
-const Course = ({ course }) => {
-  return(
-    <>
-      <Header name = { course.name }/>
-      <Content parts = { course.parts } />
-      <Total parts = { course.parts }/>
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const sum = parts.reduce((total, part) => total + part.exercises, 0)
-  return <p>Total of {sum} exercises</p>
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
