790e01bb16d87707949a53443af89b775f2d63f6
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-03-19 16:49:22 -0400

    E3.21 Deploying the database backend to production. Feature(fullstack): integrate frontend production build into backend for Render deployment
    - Generate a production build of the frontend and copied it to the backend directory
    - Configure Express to serve static files from the frontend build
    - Verifie full-stack app works locally at http://localhost:3001/
    - Prepare backend repository for deployment on Render (frontend served via backend)

--- part3/E3.1PhonebookBackend/frontend/src/components/Persons.jsx
+++ part3/E3.1PhonebookBackend/frontend/src/components/Persons.jsx
@@ -1,21 +1,18 @@
-const Persons = ({ persons , handleDelete }) => (
+const Persons = ({ persons, handleDelete }) => (
   <>
-    {persons.map(person => (
-      <Person
-      key = {person.id}
-      person = {person}
-      handleDelete = { handleDelete } />
+    {persons.map((person) => (
+      <Person key={person.id} person={person} handleDelete={handleDelete} />
     ))}
   </>
-)
+);
 
-const Person = ({ person , handleDelete }) => {
-  return(
+const Person = ({ person, handleDelete }) => {
+  return (
     <p>
-      { person.name } { person.number } 
+      {person.name} {person.number} <span> </span>
       <button onClick={() => handleDelete(person.id)}>Delete</button>
     </p>
-  )
-}
+  );
+};
 
-export default Persons
+export default Persons;
