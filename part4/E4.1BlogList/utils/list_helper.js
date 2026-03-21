9e9da27bdd2ad1a86a4a59d85beb3b7423d819f0
RepositoryCris <cris.servicios.ingenieria@gmail.com>
2026-03-20 21:31:17 -0400

    E4.4: Helper Functions and Unit Tests Step 2 totalLikes implementation and refined test suite

--- part4/E4.1BlogList/utils/list_helper.js
+++ part4/E4.1BlogList/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogList) => {
+  const reduce = (accumulator, item) => {
+    return accumulator + item.likes;
+  };
+
+  return blogList.length === 0 ? 0 : blogList.reduce(reduce, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
